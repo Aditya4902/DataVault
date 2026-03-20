a41759212f3ec9488644ee72dd9bcc83bc782f77
Aditya kumar shah <144978449+Aditya4902@users.noreply.github.com>
2026-03-20 15:04:22 +0530

    Update init.js

--- JS/init.js
+++ JS/init.js
@@ -1,15 +1,9 @@
+// INIT
+loadDB();
+if(DB.length === 0){
+  DB = [...EXAMPLE_DATA];
+  saveDB();
+}
+refresh();
 
-
-(function bootstrap() {
-  // 1. Restore any previously saved records
-  loadDB();
-
-  // 2. Seed example data only on first visit
-  if (DB.length === 0) {
-    DB = [...EXAMPLE_DATA];
-    saveDB();
-  }
-
-  // 3. Render everything
-  refresh();
-})();
+</script>
